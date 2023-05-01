dbbaa0be88b14ae7f4d498da9cf1893de1c41809
Anirudh <anirudhkandwal123@gmail.com>
2023-05-01 19:15:29 +0530

    Stripe correction

--- src/stripe/stripe.utils.js
+++ src/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H'
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
