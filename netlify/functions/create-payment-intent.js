2bb5d0b2dc78b95ced8a3cdaaf63426e0e9e0eab
Anirudh <anirudhkandwal123@gmail.com>
2023-05-04 17:06:29 +0530

    stripe amount

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     const { amount } = JSON.parse(event.body);
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount,
+      amount :amount,
       currency: "usd",
       payment_method_types: ["card"],
     });
@@ -16,7 +16,7 @@ exports.handler = async (event) => {
       body: JSON.stringify({ paymentIntent }),
     };
   } catch (error) {
-    console.log(`hi ${ error }`);
+    console.log( { error });
 
     return {
       statusCode: 400,
